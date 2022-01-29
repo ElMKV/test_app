b44eeb5cfa5c6d1f95c6c608a530f39944ba0bb6
Илья <ilya.makoveev021199@gmail.com>
2022-01-29 18:50:51 +0500

    pg

--- src/components/EmployersTable/EmployersTable.jsx
+++ src/components/EmployersTable/EmployersTable.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import EditEmployer from "../EditEmployer/EditEmployer";
+import Pagination from "../Pagination/Pagination";
 
 
 const EmployersTable = () => {
@@ -12,6 +13,9 @@ const EmployersTable = () => {
     const classes = useStyle();
     const dispatch = useDispatch()
     const employers = useSelector((state) => state.employers.employers)
+
+      const [posts, setPosts] = useState([]);
+      const [error, setError] = useState("");
 
     useEffect(() => {
         dispatch(getEmployers())
@@ -37,29 +41,37 @@ const EmployersTable = () => {
           <div>Задачи</div>
         </div>
         <div>
-          {employers.length > 0 &&
-            employers.map((item) => (
-              <div key={item.id} className={classes.employersTableRow}>
-                <div>{item.id}</div>
-                <div>{item.fio}</div>
-                <div>{item.boss !== null ? item.boss.fio : "-"}</div>
-                <div>{item.companyName}</div>
-                <div>{item.tasksCount}</div>
+          <Pagination
+            data={employers}
+            RenderComponent={
+              employers.length > 0 &&
+              employers.map((item) => (
+                <div key={item.id} className={classes.employersTableRow}>
+                  <div>{item.id}</div>
+                  <div>{item.fio}</div>
+                  <div>{item.boss !== null ? item.boss.fio : "-"}</div>
+                  <div>{item.companyName}</div>
+                  <div>{item.tasksCount}</div>
 
-                <button
-                  style={{ backgroundColor: "red", color: "white" }}
-                  onClick={() => dispatch(removeEmployers(item.id))}
-                >
-                  Удалить
-                </button>
-                <button
-                  style={{ backgroundColor: "green", color: "white" }}
-                  onClick={() => handleEdit(item.id)}
-                >
-                  Редактировать
-                </button>
-              </div>
-            ))}
+                  <button
+                    style={{ backgroundColor: "red", color: "white" }}
+                    onClick={() => dispatch(removeEmployers(item.id))}
+                  >
+                    Удалить
+                  </button>
+                  <button
+                    style={{ backgroundColor: "green", color: "white" }}
+                    onClick={() => handleEdit(item.id)}
+                  >
+                    Редактировать
+                  </button>
+                </div>
+              ))
+            }
+            title="Posts"
+            pageLimit={5}
+            dataLimit={10}
+          />
         </div>
         {currentId > 0 && (
           <EditEmployer
